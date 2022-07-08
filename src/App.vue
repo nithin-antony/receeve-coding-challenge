ca9a03c88d83820ee3f0e9f0b8499360452bc4be
Nithin Antony <nithin@Nithins-MacBook-Air.local>
2022-07-08 20:42:06 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="challenge">
+    <Dashboard msg="Welcome to the challenge 🐰" />
+  </div>
+</template>
+
+<script lang="ts">
+import Dashboard from "./components/Dashboard/Dashboard.vue";
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({ components: { Dashboard } })
+export default class App extends Vue {
+  mounted(): void {
+    console.log("let's go");
+  }
+}
+</script>
+
+<style>
+.challenge {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  color: #2c3e50;
+}
+
+#company-logo {
+  width: 120px;
+}
+</style>
---
+++ src/components/Dashboard/Dashboard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>Links of interest</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+@Component
+export default class Dashboard extends Vue {
+  @Prop()
+  msg!: string;
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
