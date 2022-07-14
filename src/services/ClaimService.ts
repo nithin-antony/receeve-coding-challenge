5f595750a2414d9cdee46486140551992718ad51
Nithin Antony <nithin.antony@outlook.in>
2022-07-14 15:04:52 +0530

    Adding env files

--- src/services/ClaimService.ts
+++ src/services/ClaimService.ts
@@ -1,9 +1,11 @@
+const baseURL: string = process.env.VUE_APP_BASE_URL || "http://localhost:9001";
+
 export async function getAllAccounts() {
-  const response = await fetch("http://localhost:9001/accounts");
+  const response = await fetch(`${baseURL}/accounts`);
   return await response.json();
 }
 
 export async function getAllClaims() {
-  const response = await fetch("http://localhost:9001/claims");
+  const response = await fetch(`${baseURL}/claims`);
   return await response.json();
 }
